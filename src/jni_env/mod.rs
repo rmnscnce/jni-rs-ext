71984f9d89788f964d0be8f6e01ca8b6d8185a1e
Paskal Sitepu <rmnscnce@ya.ru>
2024-10-25 05:56:28 +0700

    jni_env: Fix source error due to malformed linter feedback
    Signed-off-by: Paskal Sitepu <rmnscnce@ya.ru>

--- src/jni_env/mod.rs
+++ src/jni_env/mod.rs
@@ -1,9 +1,10 @@
 use jni::{
     errors::{Error, Result},
     objects::JString,
-    strings::JNIString,
     JNIEnv,
 };
+
+use crate::Sealed;
 
 impl Sealed for JNIEnv<'_> {}
 
@@ -13,9 +14,9 @@ trait JNIEnvInternalExt<'local> {
 
 impl<'local> JNIEnvInternalExt<'local> for JNIEnv<'local> {
     unsafe fn try_jni_vtable(&self) -> Result<&'local jni::sys::JNINativeInterface_> {
-        self.get_native_interface()
+        (*self.get_native_interface())
             .as_ref()
-            .ok_or(Error::NullPtr("*JNIEnv"));
+            .ok_or(Error::NullPtr("*JNIEnv"))
     }
 }
 
@@ -31,18 +32,27 @@ impl<'local> JNIEnvExt<'local> for JNIEnv<'local> {
         &self,
         obj: &'obj_ref JString<'other_local>,
     ) -> Result<String> {
-        obj.is_null()
-            .then(|| Err(Error::NullPtr("get_string_region obj argument")))?;
+        if obj.is_null() {
+            Err(Error::NullPtr("get_string_region obj argument"))?;
+        }
 
         let len = match unsafe { self.try_jni_vtable() }?.GetStringUTFLength {
-            Some(r#fn) => unsafe { r#fn(self.get_raw(), obj.into_raw()) },
+            Some(r#fn) => unsafe { r#fn(self.get_raw(), obj.as_raw()) },
             None => Err(Error::JNIEnvMethodNotFound("GetStringUTFLength"))?,
         };
 
-        let mut buf = vec![i8; len];
+        let mut buf = vec![0; len as usize];
 
         match unsafe { self.try_jni_vtable() }?.GetStringUTFRegion {
-            Some(r#fn) => unsafe { r#fn(self.get_raw(), obj.into_raw(), 0, len, buf.as_mut_ptr()) },
+            Some(r#fn) => unsafe {
+                r#fn(
+                    self.get_raw(),
+                    obj.as_raw(),
+                    0,
+                    len,
+                    buf.as_mut_ptr().cast(),
+                )
+            },
             None => Err(Error::JNIEnvMethodNotFound("GetStringUTFRegion"))?,
         };
 
